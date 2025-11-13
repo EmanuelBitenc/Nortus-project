12b354adad80ac058fe9711a5c3b2069a8d7cbf9
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-11-13 15:19:06 -0300

    feat: criar componentes AsideImg e calsse utilitaria

---
+++ src/app/loginPage/components/aside-img.tsx
@@ -0,0 +1,33 @@
+import "../style.css";
+import Image from "next/image";
+import loginFigure from "../../../../public/imgs/loginFigure.png";
+import { ChevronDown, Headphones } from "@deemlol/next-icons";
+
+export default function AsideImg() {
+  return (
+    <div className="relative w-full h-[620px]">
+      <div className="relative w-full h-full rounded-3xl overflow-hidden shadow-2xl">
+        <Image
+          src={loginFigure}
+          alt="Ilustração"
+          fill
+          style={{ objectFit: "cover" }}
+          priority
+        />
+      </div>
+
+      <div className="absolute top-0 right-0 flex items-center gap-3 bg-(--background) px-2 py-2 rounded-bl-2xl">
+        <button className="buttonHelp">
+          <Headphones size={16} />
+          <span>Ajuda</span>
+        </button>
+
+        <button className="buttonHelp">
+          <span className="text-base">🇧🇷</span>
+          <span>PT-br</span>
+          <ChevronDown size={16} />
+        </button>
+      </div>
+    </div>
+  );
+}
