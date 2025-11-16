2e5ea338157ce248f9fab4bf300c38db6a8e9f89
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-11-16 20:13:24 -0300

    fix: autentiacção erro

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,12 +10,6 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    if (typeof window !== "undefined") {
-      const token = cookieStorage.getToken();
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-    }
     return config;
   },
   (error) => {
